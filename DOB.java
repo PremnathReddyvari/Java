7df52008fc3af8f8ec4be9fd7d3f85687510c45a
PremnathReddyvari <43442577+PremnathReddyvari@users.noreply.github.com>
2018-10-09 18:35:20 +0530

    Task 5 (DOB)
    Java Program to accept date of birth in dd-mm-yyyy format as command line arguments and calculate age of the respective person.

---
+++ DOB.java
@@ -0,0 +1,24 @@
+package second;
+
+import java.text.ParseException;
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Scanner;
+
+public class DOB {
+	public static void main(String[] args) throws ParseException {
+		@SuppressWarnings("resource")
+		Scanner birthDate = new Scanner(System.in);
+		System.out.print("Enter date of birth in the format dd-mm-yyyy: ");
+		String input2 = birthDate.next();
+		String[] split1 = input2.split("-");
+		LocalDate today = LocalDate.now();
+		LocalDate birthday = LocalDate.of(Integer.parseInt(split1[2]), Integer.parseInt(split1[1]),
+				Integer.parseInt(split1[0]));
+		Period p = Period.between(birthday, today);
+		System.out.println(p.getDays() + " years");
+		System.out.println(p.getMonths() + " months");
+		System.out.println(p.getYears() + " days");
+	}
+
+}
